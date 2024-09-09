6e369c9ae8b5c0bdc479405f2edf886204e061ce
Yan Smaliak <ysmaliak@gmail.com>
2024-09-09 17:24:59 +0200

    Initial Commit

---
+++ tca-navigation-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tca-navigation-test
+//
+//  Created by Yan Smaliak on 09/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
